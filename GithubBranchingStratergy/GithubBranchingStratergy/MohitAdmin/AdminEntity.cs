2b6ce704228add1d204c342690fca655026b931b
Mohit Kumar Jhariya <mohitkumarj5@gmail.com>
2017-02-25 10:20:54 +0530

    #Mohit: AdminEntity Added

---
+++ GithubBranchingStratergy/GithubBranchingStratergy/MohitAdmin/AdminEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MohitAdmin
+{
+    public class AdminEntity
+    {
+        public int AdminId { get; set; }
+        public string Name { get; set; }
+    }
+}
